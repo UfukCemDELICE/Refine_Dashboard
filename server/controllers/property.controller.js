43a0a778036d5cde20e40b04aebf35e99951169e
UfukCemDELICE <ufukcemdelice02@gmail.com>
2023-03-14 01:15:57 +0300

    server created, models, controllers and routes are done

---
+++ server/controllers/property.controller.js
@@ -0,0 +1,16 @@
+import propertyModel from "../mongodb/models/Property.js"; 
+import userModel from "../mongodb/models/User.js";
+
+const getAllProperties = async (req, res) => {};
+const getPropertyDetail = async (req, res) => {};
+const createProperty = async (req, res) => {};
+const updateProperty = async (req, res) => {};
+const deleteProperty = async (req, res) => {};
+
+export {
+  getAllProperties,
+  getPropertyDetail,
+  createProperty,
+  updateProperty,
+  deleteProperty
+};
---
+++ server/routes/property.routes.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import { 
+  getAllProperties, getPropertyDetail, createProperty, updateProperty, deleteProperty
+} from '../controllers/property.controller.js';
+
+const router = express.Router();
+
+router.route('/').get(getAllProperties);
+router.route('/').post(createProperty);
+router.route('/:id').get(getPropertyDetail);
+router.route('/:id').delete(deleteProperty);
+router.route('/:id').patch(updateProperty);
+
+export default router;
