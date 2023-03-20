3b85a1a9a2a488cfa60c07d850487972fe5175ae
UfukCemDELICE <ufukcemdelice02@gmail.com>
2023-03-21 00:06:21 +0300

    custom button, propertyCard, all-property done, property and user controllers updated

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -9,8 +9,11 @@ const createUser = async (req, res) => {
     const userExists = await User.findOne({ email });
 
     if(userExists) return res.stats(200).json(userExists);
+
     const newUser = await User.create({ name, email, avatar });
+
     res.status(200).json(newUser);
+    
   }catch (error){
     res.status(500).json({ message: error.message })
   }
